ec7cdecef8660dc4b7fa0d7eebc805a93bcc8943
Arbiter_Aryad <notanarbiter@gmail.com>
2023-07-12 04:15:23 +0530

    deletion done

--- controller/content/deletePost.js
+++ controller/content/deletePost.js
@@ -6,42 +6,28 @@ const fs = require("fs");
 const deletePost = async (req, res) => {
   const postId = req.params.id;
   try {
-    const neededPost = await postModel.findById(postId);
-    if (!neededPost) {
+    let neededPost = await pool.query('SELECT * FROM posts WHERE id = $1', [postId])
+    if (!neededPost.rowCount) {
       return Err(req, res, "Post does not exist");
     }
-    console.log(neededPost.author, neededPost.author_id);
-    const postAuthor = await userModel.findById(neededPost.authorDetails);
-    if (!postAuthor) {
-      return Err(req, res, "User not found while deleting post");
-    }
-    const oldSize = postAuthor.posts.length;
-    console.log("oldSize", oldSize);
-    postAuthor.posts = postAuthor.posts.filter((post) => {
-      return String(post) !== String(postId);
-    });
+    neededPost = neededPost.rows[0];
+    console.log("POST(TO BE DELETED) AUTHOR=", neededPost.author);
 
-    if (postAuthor.posts.length < oldSize) {
-      console.log("Removed from user posts list");
-    } else {
-      return Err(req, res, "Error while removing post from list");
-    }
-    await postAuthor.save();
     let filePath = path.join(__dirname, "../../public", neededPost.cover);
-    console.log("img path=", filePath);
+    // console.log("img path=", filePath);
 
     fs.unlink(filePath, (err) => {
-      if (err) console.error(err.message);
-      else console.log("Image deleted @", filePath);
+      if (err) console.error("ERROR IN IMG REMOVAL", err.message);
+      else console.log("IMAGE DELETED FROM", filePath);
     });
 
-    const deletedPost = await postModel.findByIdAndDelete(postId);
-    console.log("DELETED=", deletedPost);
+    const deletedPost = await pool.query("DELETE FROM posts WHERE id = $1 RETURNING *;", [postId]);
+    console.log("DELETED=", deletedPost.rows[0]);
 
     return res.send({
       status: "ok",
       message: "Post deleted successfully",
-      post: deletedPost,
+      post: deletedPost.rows[0]
     });
   } catch (err) {
     return Err(req, res, err.message);
