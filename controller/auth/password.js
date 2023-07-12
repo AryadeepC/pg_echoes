dd6e5b6a9a49cb91e32125520b81e24305cbfd75
Arbiter_Aryad <notanarbiter@gmail.com>
2023-07-12 23:24:56 +0530

    forgot and reset done for good, i hope

--- controller/auth/password.js
+++ controller/auth/password.js
@@ -4,6 +4,10 @@ const { Err } = require("../../utils/ErrorResponse");
 const { transporter, mailBody, uid } = require("../../utils/sendEmail");
 const bcrypt = require("bcryptjs");
 
+bcrypt.compare("e55106", "$2a$08$O2cC9rnZMrSlp2Kq4K3dj.w2Myw5HIcQF/j7vtkOVlZo12YmYdNCC").then(res => {
+  console.log(res);
+}).catch(err => console.error(err.message));
+
 const forgotPassword = async (req, res) => {
   try {
     const foundUser = await pool.query(`SELECT * FROM users WHERE id = $1 LIMIT 1`, [req.userId]);
@@ -18,7 +22,7 @@ const forgotPassword = async (req, res) => {
       subject: "Verfication mail for password reset from Echoes",
       html: mailBody,
     };
-
+    console.log("OG RESET TOKEN=", uid);
     const otp = await bcrypt.hash(uid, 8);
 
     const userWithCode = await pool.query('UPDATE users SET reset_password_token = $1,reset_password_expiry = $2 WHERE id = $3 RETURNING *;', [otp, Date.now() + 2 * 60 * 1000, req.userId])
@@ -40,32 +44,28 @@ const forgotPassword = async (req, res) => {
 };
 
 const resetPassword = async (req, res) => {
+  console.log("USER=", req.userId);
   const { code = "", password = "" } = req.body;
   if (!code || !password)
     return Err(req, res, "Missing credentials in reset password");
 
   try {
     const hashedpassword = await bcrypt.hash(password, 8);
-    const foundUser = await userModel.findById(req.userId);
-    if (!foundUser) {
+    const foundUser = await pool.query('SELECT reset_password_token, reset_password_expiry FROM users WHERE id = $1', [req.userId])
+    if (!foundUser.rowCount) {
       return Err(req, res, "User not found");
     }
-    if (foundUser.resetPasswordExpiry < Date.now()) {
+    if (foundUser.rows[0].reset_password_expiry < Date.now()) {
       return Err(req, res, "Code Expired !!");
     }
-    const isMatch = await bcrypt.compare(foundUser.resetPasswordToken, code);
+    console.log("ENTERED RESET TOKEN=", code);
+    const isMatch = await bcrypt.compare(code, foundUser.rows[0].reset_password_token);
     if (!isMatch) {
       return Err(req, res, "Verification Code Mismatch !!");
     }
 
-    const newUser = await userModel.findByIdAndUpdate(
-      req.userId,
-      {
-        password: hashedpassword,
-      },
-      { new: true }
-    );
-    console.log(newUser);
+    const newUser = await pool.query('UPDATE users SET password = $1,reset_password_token = NULL, reset_password_expiry = NULL WHERE id = $2 RETURNING *;', [hashedpassword, req.userId]);
+    console.log(newUser.rowCount ? "PASSWORD CHANGED" : "COULDN'T CHANGE PASSWORD");
 
     return res.send({
       status: "ok",
@@ -75,5 +75,7 @@ const resetPassword = async (req, res) => {
     return Err(req, res, error.message);
   }
 };
+
+
 
 module.exports = { forgotPassword, resetPassword };
