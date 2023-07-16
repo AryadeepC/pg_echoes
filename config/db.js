43b1b588804fd7489b4e999a3dee1375703ec4fd
Arbiter_Aryad <notanarbiter@gmail.com>
2023-07-16 21:24:29 +0530

    fix

--- config/db.js
+++ config/db.js
@@ -2,17 +2,17 @@ const path = require("path");
 require("dotenv").config({ path: path.join(__dirname, "../config.env") });
 const { Pool } = require('pg')
 
-const pool = new Pool({
-  host: process.env.POSTGRES_HOST_LOCAL,
-  user: process.env.POSTGRES_USER_LOCAL,
-  password: process.env.POSTGRES_PASSWORD_LOCAL,
-  port: process.env.POSTGRES_PORT_LOCAL,
-  database: process.env.POSTGRES_DB_LOCAL,
-})
-
 // const pool = new Pool({
-//   connectionString: process.env.POSTGRES_URL + "?sslmode=require",
+//   host: process.env.POSTGRES_HOST_LOCAL,
+//   user: process.env.POSTGRES_USER_LOCAL,
+//   password: process.env.POSTGRES_PASSWORD_LOCAL,
+//   port: process.env.POSTGRES_PORT_LOCAL,
+//   database: process.env.POSTGRES_DB_LOCAL,
 // })
+
+const pool = new Pool({
+  connectionString: process.env.POSTGRES_URL + "?sslmode=require",
+})
 
 const poolAlive = async () => {
   const tm = await pool.query('SELECT NOW()');
